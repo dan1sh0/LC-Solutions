886c1db2bbb7ff73321e2026bde6328453e61cbb
Danish Ghumman <114195774+dan1sh0@users.noreply.github.com>
2024-10-21 15:56:17 -0400

    Time: 1 ms (97.36%), Space: 16.5 MB (94.95%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        # we can have two pointers 
+        # one at s and another at t 
+        # we check if char is equal to each other at both s and t 
+        # if it is we increment both pointers and continue 
+        # if they are not equal we just increment t, and continue 
+        # if we are able to reach the end of both strings we return True 
+        # if we could not, return False 
+        
+        
+        char1, char2 = 0, 0 
+        
+        while char1 < len(s) and char2 < len(t):
+            if s[char1] == t[char2]:
+                char1+=1
+                char2+=1
+            else:
+                char2+=1
+            
+        return len(s) == char1
